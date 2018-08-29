83aa42f34673cf4517a1444cb5e11ae7bc72df31
Chris <Chris.P.O'Riordan@aib.ie>
2018-08-29 11:40:03 +0100

    Experimenting with the use of Webviews

---
+++ app/src/main/java/com/example/standard/webviews/WebActivity.java
@@ -0,0 +1,31 @@
+package com.example.standard.webviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        webView = findViewById(R.id.webView);
+        webView.loadUrl("https://www.google.com/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()){
+            webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
